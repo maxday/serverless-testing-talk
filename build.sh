67aefe606615d31d5994cee72e0c4a09b3499dd9
Maxime David <maxime.david@datadoghq.com>
2023-03-01 12:13:37 -0500

    add build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker buildx build --platform linux/amd64 -t maxday/pizza . --load
+docker buid --platform linux/amd64 -t maxday/pizza .
 dockerId=$(docker create --platform linux/amd64 maxday/pizza)
 docker cp $dockerId:/bootstrap.zip .
