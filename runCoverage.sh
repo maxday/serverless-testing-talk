bbd0dfdc0a3a9f92f009309f0dd59bbfa931f331
Maxime David <maxime.david@datadoghq.com>
2023-05-31 09:21:54 +0200

    coverage

--- runCoverage.sh
+++ runCoverage.sh
@@ -1,4 +1,4 @@
 mkdir -p target/coverage/html
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='./target/cargo-test-%p-%m.profraw' cargo test
-grcov ./target --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
+CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='./target/cargo-test-%p-%m.profraw' cargo test --bins
+grcov ./target --binary-path ./target/debug/deps/ -s ./src -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
 open target/coverage/html/index.html
