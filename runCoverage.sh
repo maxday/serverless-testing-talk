0c04cc04ee5fee69aa5d89dc1981d92c9455ec08
Maxime David <maxime.david@datadoghq.com>
2023-11-12 22:14:51 -0500

    update coverage

--- runCoverage.sh
+++ runCoverage.sh
@@ -1,15 +1,3 @@
-# export CARGO_INCREMENTAL=0
-# export RUSTFLAGS="-Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off"
-# export RUSTDOCFLAGS="-Cpanic=abort"
-
-# mkdir -p target/coverage/html
-# cargo build
-# cargo test --bins
-# grcov . -s . \
-# --binary-path ./target/debug/ -t html \
-# --branch --ignore-not-existing -o ./target/debug/coverage/
-# open target/coverage/html/index.html
-
 mkdir -p target/coverage/html
 CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='target/cargo-test-%p-%m.profraw' cargo test --bins
 grcov . -s src \
